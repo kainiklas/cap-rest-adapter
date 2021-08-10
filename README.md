75f3db94021401ca97e149c721bf51e20f58d893
Kai Niklas <kai.niklas@web.de>
2021-08-10 14:47:02 +0200

    enhancements

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ Reasons why you want or need to create such an adapter:
 
 - Specific architecture requirements, e.g., harmonization of how services look like accross the enterprise
 - Access a service using different URL patterns, i.e., `ressource/{id}` instead of `ressource('{id}')`
-- Change structure and content of the payload, e.g., delete, add, change, rename, combine attributes
+- Change structure and content of the payload, e.g., delete, add, change, rename, combine, etc. of fields
 - Change the standard OData query paramater names, e.g., pagination with `?pageSize` and `?offset` instead of `$top` and `$skip`
-- Reuse existing services without re-implementing them, e.g., OData services but also classic non-compliant REST or WS Services
+- Reuse existing services without re-implementing them, e.g., OData services but also REST or classic "non-compliant" WS Services
 
-It contains these folders and files, following our recommended project layout:
+## Project Structure
 
 | File or Folder | Purpose                                               |
 | -------------- | ----------------------------------------------------- |
@@ -19,7 +19,7 @@ It contains these folders and files, following our recommended project layout:
 | `package.json` | project metadata and configuration                    |
 | `.cdsrc.json`  | service destination configuration                     |
 
-## Run the Demo
+## Run the Code
 
 - Open a new terminal and run `cds watch`
 - Open <http://localhost:4004/northwind/Categories>
@@ -30,4 +30,4 @@ It contains these folders and files, following our recommended project layout:
 
 ## Learn More
 
-Learn more at https://cap.cloud.sap/docs/get-started/.
+Learn more at <https://cap.cloud.sap/docs/get-started/>.
--- srv/NorthWindService.js
+++ srv/NorthWindService.js
@@ -1,40 +1,53 @@
 const cds = require("@sap/cds");
+const objectMapper = require("object-mapper");
 
 module.exports = cds.service.impl(async function () {
   const { Categories } = this.entities;
   const service = await cds.connect.to("NorthWind");
 
   this.on("READ", Categories, async (req) => {
-    const categories = await service.tx(req).run(req.query);
-
-    let transformedCategories;
-
-    if (Array.isArray(categories)) {
-      transformedCategories = transformObjectArray(categories);
-    } else {
-      transformedCategories = transformObject(categories);
+    // ?pagesize instead of OData $top
+    if (req.req.query.pageSize) {
+      req.query.SELECT.limit.rows = { val: parseInt(req.req.query.pageSize) };
     }
 
-    return transformedCategories;
+    // ?offeset instead of OData $skip
+    if (req.req.query.offset) {
+      req.query.SELECT.limit.offset = { val: parseInt(req.req.query.offset) };
+    }
+
+    // execute the query
+    const categories = await service.tx(req).run(req.query);
+
+    // multiple objects
+    if (Array.isArray(categories)) {
+      return categories.map((o) => transformObject(o));
+    }
+
+    // one object
+    //return transformObject(categories);
+    return mapObject(categories);
   });
 
   let transformObject = function (o) {
-    let keys = Object.keys(o);
-    var transformedObject = {};
+    // add field
+    o.New = "Some value";
 
-    keys.forEach((key) => {
-      transformedObject[key.toLowerCase()] = o[key];
-    });
+    // remove field
+    delete o.Picture;
 
-    return transformedObject;
+    // change field
+    o.Description = "Legacy: " + o.Description;
+
+    return o;
   };
 
-  let transformObjectArray = function (arr) {
-    let transArr = [];
-    arr.forEach((o) => {
-      transArr.push(transformObject(o));
-    });
+  let mapObject = function (o) {
+    let map = {
+      CategoryID: "wrapper.categories.id",
+      CategoryName: "wrapper.categories.name",
+    };
 
-    return transArr;
+    return objectMapper(o, map);
   };
 });
